0fd299061dbaffa631e41433201bcd4ea2935908
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-14 10:31:20 +0530

    Add files via upload

---
+++ Design6.py
@@ -0,0 +1,10 @@
+#MAKE BY SUMIT 
+import turtle as t
+t.bgcolor('black')
+t.pencolor("orange")
+t.speed('fastest')
+for i in range (450):
+    t.forward(i)
+    t.left(124)
+t.hideturtle()
+t.done()
---
+++ Design7.py
@@ -0,0 +1,10 @@
+#MAKE BY SUMIT 
+import turtle as t
+t.bgcolor('black')
+t.pencolor("orange")
+t.speed('fastest')
+for i in range (430):
+    t.forward(i)
+    t.left(190)
+t.hideturtle()
+t.done()
